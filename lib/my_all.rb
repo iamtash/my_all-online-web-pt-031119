e918c4d35e4a5a4c511779aea472cb37098bccd0
Natasha Chernis <natashachernis@gmail.com>
2019-03-23 02:16:55 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,10 +4,10 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
-    binding.pry
     block_return_values << yield(collection[i])
     i += 1
   end
+  binding.pry
 end
-binding.pry
+
 my_all?([1,2,3]) {|i| i < 2}
